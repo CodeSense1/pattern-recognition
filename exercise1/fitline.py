6162b2793700cf32d1c66e9592ef65bb3e82c236
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2020-09-03 23:50:21 +0300

    Added better line updation

--- exercise1/fitline.py
+++ exercise1/fitline.py
@@ -18,56 +18,77 @@ def linregress(xd,yd):
     
     return a,b
 
-# Define x,y points
-x = []
-y = []
-
 def onclick(evt):
-    print(f"button ({evt.button}), x=({evt.x}), y=({evt.y}), xdata=({evt.xdata}), ydata=({evt.ydata})")
+    # print(f"button ({evt.button}), x=({evt.x}), y=({evt.y}), xdata=({evt.xdata}), ydata=({evt.ydata})")
     
     # Left mouse button
     if evt.button == 1 and evt.xdata is not None and evt.ydata is not None:
-        fig.clear()
-        ax.set_xlim([-2, 5])
-        ax.set_ylim([-2, 5])
-        plotPoints(evt.xdata, evt.ydata)        
+        
+        plotPoints(evt.xdata, evt.ydata)
+        drawline(x,y)        
     
     # Right mouse button
     elif evt.button == 3:
-        plt.close()
+        plt.close(fig)
+
+        
         
 
 def plotPoints(xdata,ydata):
     # add new point and draw all previously added points
     x.append(xdata)
     y.append(ydata)
-    plt.plot(x, y, 'kx')
-    drawline(x,y)
+    plt.plot(xdata, ydata, 'kx')
     fig.canvas.draw()
 
 def drawline(x,y):
 
     if len(x) < 2 or len(y) < 2:
-        print("x or y has too few values!")
         return
 
+    
     a,b = linregress(x,y)
     
-    print(f"My fit: a = {a} and b = {b}")
+    print("My fit: a = {:.4f} and b = {:.4f}".format(a,b))
 
-    xp = np.arange(-2,5,0.1)
-    plt.plot(xp, a*xp + b, 'r-')
-    plt.legend(["points", "Regression"])
+    xp = np.arange(0, 10,0.1)
+    
+    regression.set_data(xp, a*xp + b)
+
+    aText.set_text("a = {:.4f}".format(a))
+    bText.set_text("b = {:.4f}".format(b))
+
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    plt.legend(["Regression", "points"])
 
 
 def init():
     fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(111)
-    ax.set_xlim([-2, 5])
-    ax.set_ylim([-2, 5])
+    ax.set_xlim([0, 10])
+    ax.set_ylim([0, 10])
+
+
+    dx = np.arange(0, 10,0.1)
+    dy = dx
+    regLine, = plt.plot(dx, dy,'r-')
+    regLine.set_figure(fig)
+
+    aText = plt.text(0.5,8, "a = 1")
+    bText = plt.text(0.5,7.7, "b = -2") 
+
+    plt.legend(["Regression"])
+
     fig.canvas.mpl_connect('button_press_event', onclick)
-    return fig, ax
+    return fig, ax, regLine, aText, bText
 
 
-fig, ax = init()
+
+# Define x,y points
+x = []
+y = []
+
+fig, ax, regression, aText, bText = init()
 plt.show()
+
